1ca701d624e52d189e8972727bec9502e772b40c
Samigo480 <samigo480@gmail.com>
2023-06-15 10:24:26 +0300

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - prints "Programming is like building a multilingual puzzle, followed by a new line.
+ * main - prints a text
  *
  * Return: Always 0.
  */
