2104a7fe0d08aac7d6d709107a6674b7db05ac4a
Samigo480 <samigo480@gmail.com>
2023-06-15 13:30:05 +0300

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,23 @@
-#include <stdio.h>
+#include <studio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 if exists properly, non-zero otherwise
+ * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
-	printf("Size of a char: %d bite(s)\n", sizeof(char));
-	printf("Size of an int: %d bite(s)\n", sizeof(int));
-	printf("Size of a long int: %d bite(s)\n", sizeof((long int));
-	printf("Size of a long long int: %d bite(s)\n, sizof(long long int));
-	printf("Size of a float: %d bite(s)\n", sizeof(float));
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float e;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n" (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
