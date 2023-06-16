ad542d3aadf723cb55ec2f04f48f1f9828d33d79
Samigo480 <samigo480@gmail.com>
2023-06-16 12:32:26 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print the number from 0 upto 9.
+ *
+ * Retutn: 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
