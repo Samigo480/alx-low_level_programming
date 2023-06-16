9b6c98cff52f96f589150a94d43c7ffd1fc1ae44
Samigo480 <samigo480@gmail.com>
2023-06-16 14:01:42 -0700

    Task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x, y, z;
+
+	x = 48;
+	y = 49;
+	z = 50;
+	while ((x < 56) && (y < 57) && (z < 58))
+	{
+		putchar(x);
+		putchar(y);
+		putchar(z);
+		if ((x == 55) && (y == 56) && (z == 57))
+		{
+			putchar('\n');
+			x++;
+			y++;
+			z++;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+			if (z < 57)
+			{
+				z++;
+			}
+			else if ((y < 56) && (z == 57))
+			{
+				y == 1 + ++y;
+			}
+			else
+			{
+				y = ++x + 1;
+				z = y + 1;
+			}
+		}
+	}
+	return (0);
+}
