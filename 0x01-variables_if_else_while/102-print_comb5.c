15ac9e379c216a4c094f7775109b41a388e550cd
Samigo480 <samigo480@gmail.com>
2023-06-16 14:33:19 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,7 +18,7 @@ int main(void)
 			putchar((' '));
 			putchar((z / 10) + '0');
 			putchar((z % 10) + '0');
-			if ((y == 98) && (z == 99))
+			if ((y = 98) && (z = 99))
 					continue;
 			putchar(',');
 			putchar(' ');
