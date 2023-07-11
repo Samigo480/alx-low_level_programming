bcbb7d4b6592005718c7545dbf42ae9624056876
Smigo480 <samigo480@gmail.com>
2023-07-11 10:24:17 +0300

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,20 +11,20 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *strout;
-	unsigned int x, y, z, limit;
+	unsigned int i, j, k, limit;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	for (i = 0; s1[x] != '\0'; x++)
+	for (i = 0; s1[i] != '\0'; i++)
 		;
 
-	for (j = 0; s2[y] != '\0'; y++)
+	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	strout = malloc(sizeof(char) * (x + y + 1));
+	strout = malloc(sizeof(char) * (i + j + 1));
 
 	if (strout == NULL)
 	{
@@ -32,12 +32,12 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	for (z = 0; z < x; z++)
-		strout[z] = s1[z];
+	for (k = 0; k < i; k++)
+		strout[k] = s1[k];
 
-	limit = y;
-	for (y = 0; y <= limit; z++, y++)
-		strout[z] = s2[y];
+	limit = j;
+	for (j = 0; j <= limit; k++, j++)
+		strout[k] = s2[j];
 
 	return (strout);
 }
